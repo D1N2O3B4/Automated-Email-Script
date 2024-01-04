7a24919dcebeb88d7623e7456bebdbd90bb9b47b
d1n2o3b4 <davebostons2017@gmail.com>
2024-01-04 14:12:27 +0000

    made a change in receipient mail

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# import smtplib
-# from email.message import EmailMessage
-
-# email = EmailMessage()
-# print(email)
-# email['from'] = 'Python <dizzymonde23@proton.me>'
-# email['to'] = 'davebostons2017@gmail.com'
-# email['subject'] = 'Automated Message Test'
-
-# email.set_content("Ok so if youre seeing this, a message was sent from another email using a python script")
-
-# with smtplib.SMTP(host='localhost',port=1234) as smtp:
-#     smtp.ehlo()
-#     smtp.starttls()
-#     smtp.ehlo()
-
-#     smtp.login('dizzymonde23@proton.me','Isaac@2001')
-#     smtp.send_message(email)
-#     print("Mail sent......")
-###################################
 import getpass
 import smtplib
 
@@ -25,7 +5,7 @@ import smtplib
 from_mail = "davidboateng68@outlook.com"
 password = getpass.getpass("Type password: ")
 
-to_mail = "dizzymode23@proton.me"
+to_mail = "dizzymonde23@proton.me"
 
 message = "Hello this is a test email"
 
